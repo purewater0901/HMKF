7f9995deea801673a0b2016a1b3c40ed3cd9b4d3
yutaka <purewater0901@gmail.com>
2023-03-01 01:41:20 +0900

    update simulation

--- scripts/visualizer/simulation_result_visualizer.py
+++ scripts/visualizer/simulation_result_visualizer.py
@@ -8,12 +8,18 @@ import platform
 import os
 
 if __name__ == '__main__':
+    dt = 0.2
     data_num = 30
     os.chdir('../../')
     path = os.getcwd()
     data = []
     for i in range(data_num):
         data.append(pd.read_csv(path + "/result/data/simulation_result/result_monte_carlo_" + str(i) +  ".csv"))
+
+    times = []
+    ini_data = data[0]
+    for i in range(len(ini_data)):
+        times.append(ini_data['time'][i])
 
     ekf_xy_rmse = []
     ukf_xy_rmse = []
@@ -50,35 +56,37 @@ if __name__ == '__main__':
         mkf_yaw_ame.append(mkf_yaw_ame_sum/len(data))
         hmkf_yaw_ame.append(hmkf_yaw_ame_sum/len(data))
 
-    fig = plt.figure(figsize=(15.5,8.5))
-    plt.subplots_adjust(wspace=0.4, hspace=0.8)
+    fig = plt.figure(figsize=(20.5,20.5))
+    #plt.subplots_adjust(wspace=0.4, hspace=0.3)
+    plt.rcParams['pdf.fonttype'] = 42
+    plt.rcParams['ps.fonttype'] = 42
     plt.rcParams['mathtext.fontset'] = 'stix'
     plt.rcParams["font.size"] = 28
-    plt.rcParams['xtick.labelsize'] = 30
-    plt.rcParams['ytick.labelsize'] = 30
+    plt.rcParams['xtick.labelsize'] = 35
+    plt.rcParams['ytick.labelsize'] = 35
     plt.rcParams['xtick.direction'] = 'in' # x axis in
     plt.rcParams['ytick.direction'] = 'in' # y axis in
     plt.rcParams['font.family'] = 'Times New Roman'
 
-    ax1 = fig.add_subplot(121)
-    ax1.plot(hmkf_xy_rmse, color="black", linewidth=2.0, label="MKF", linestyle="solid")
-    ax1.plot(mkf_xy_rmse, color="red", linewidth=2.0, label="MKF", linestyle="dotted")
-    ax1.plot(ekf_xy_rmse, color="blue", linewidth=1.5, label="EKF", linestyle="dashed")
-    ax1.plot(ukf_xy_rmse, color="green", linewidth=1.5, label="UKF", linestyle="dashdot")
-    ax1.set_xlabel(r"time step", fontsize=40)
-    ax1.set_ylabel(r"position error [m]", fontsize=40)
-    ax1.legend(fontsize=25)
-    ax1.set_title("Position RMSE")
+    ax1 = fig.add_subplot(211)
+    ax1.plot(times, hmkf_xy_rmse, color="black", linewidth=6.0, label="HMKF", linestyle="solid")
+    ax1.plot(times, mkf_xy_rmse, color="red", linewidth=6.0, label="MKF", linestyle="dotted")
+    ax1.plot(times, ekf_xy_rmse, color="blue", linewidth=5.5, label="EKF", linestyle="dashed")
+    ax1.plot(times, ukf_xy_rmse, color="green", linewidth=5.5, label="UKF", linestyle="dashdot")
+    ax1.set_ylabel(r"Position RMSE [m]", fontsize=38)
+    ax1.legend(fontsize=30)
+    #ax1.set_title("Position RMSE")
 
-    ax2 = fig.add_subplot(122)
-    ax2.plot(hmkf_yaw_ame, color="black", linewidth=2.0, label="MKF", linestyle="solid")
-    ax2.plot(mkf_yaw_ame, color="red", linewidth=2.0, label="MKF", linestyle="dotted")
-    ax2.plot(ekf_yaw_ame, color="blue", linewidth=1.5, label="EKF", linestyle="dashed")
-    ax2.plot(ukf_yaw_ame, color="green", linewidth=1.5, label="UKF", linestyle="dashdot")
-    ax2.set_xlabel(r"time step", fontsize=40)
-    ax2.set_ylabel(r"yaw angle error", fontsize=40)
-    ax2.legend(fontsize=25)
-    ax1.set_title("Yaw Angle AME")
+    ax2 = fig.add_subplot(212)
+    ax2.plot(times, hmkf_yaw_ame, color="black", linewidth=2.0, label="HMKF", linestyle="solid")
+    ax2.plot(times, mkf_yaw_ame, color="red", linewidth=2.0, label="MKF", linestyle="dotted")
+    ax2.plot(times, ekf_yaw_ame, color="blue", linewidth=1.5, label="EKF", linestyle="dashed")
+    ax2.plot(times, ukf_yaw_ame, color="green", linewidth=1.5, label="UKF", linestyle="dashdot")
+    ax2.set_xlabel(r"time [s]", fontsize=40)
+    ax2.set_ylabel(r"Yaw AME", fontsize=38)
+    #ax2.legend(fontsize=25)
+    #ax2.set_title("Yaw Angle AME")
 
+    plt.savefig(path + "/result/picture/simulation_result/simulation_result.eps", bbox_inches="tight", pad_inches=0.05)
     plt.show()
 
